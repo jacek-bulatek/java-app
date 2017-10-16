602c890d923680e7293c54f3afea499b7ec9cf2e
jacek-bulatek <jacek.bulatek@gmail.com>
2017-10-16 15:53:38 +0200

    Last failed build: fails commented part of setLayout() method

--- app/src/main/java/com/example/nockanakalinowej/LvlMng.java
+++ app/src/main/java/com/example/nockanakalinowej/LvlMng.java
@@ -7,7 +7,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import static java.lang.Math.min;
 
@@ -18,7 +22,7 @@ import static java.lang.Math.min;
  */
 
 class LvlMng {
-    AppCompatActivity window;
+    Level window;
     ConstraintLayout lvlLayout;
     int minmarginx;
     int minmarginy;
@@ -29,7 +33,7 @@ class LvlMng {
     int height;
     int[] idList;
 
-    public LvlMng(AppCompatActivity context, int LvlNo){
+    public LvlMng(Level context, int LvlNo){
         idList = new int[]{
             R.id.kafel1, R.id.kafel2, R.id.kafel3, R.id.kafel4, R.id.kafel5,
             R.id.kafel6, R.id.kafel7, R.id.kafel8, R.id.kafel9, R.id.kafel10,
@@ -56,8 +60,99 @@ class LvlMng {
         int edge = kafelEdge(kflamountx, kflamounty);
 
         ConstraintSet constraintSet = new ConstraintSet();
-        lvlLayout = (ConstraintLayout) LayoutInflater.from(window).inflate(R.layout.activity_level, null);
-        constraintSet.clone(lvlLayout);
+        lvlLayout = new ConstraintLayout(window);//(ConstraintLayout) LayoutInflater.from(window).inflate(R.layout.activity_level, null);
+        lvlLayout.setId(R.id.Level_layout);
+        //constraintSet.clone(lvlLayout);
+
+        /*
+         *    ---- SETTING CONSTANTS ----
+         */
+
+
+            //  ---- CREATING INSTANCES OF CONSTANT WIDGETS ----
+            final Button start = new Button(window);
+            final Button previous = new Button(window);
+            final Button next = new Button(window);
+            TextView counter = new TextView(window);
+            ImageView fullImage = new ImageView(window);
+
+            //  ---- SETTINGS IDs OF CONSTANT WIDGETS ----
+            start.setId(R.id.start);
+            previous.setId(R.id.previous);
+            next.setId(R.id.next);
+            counter.setId(R.id.counter);
+            fullImage.setId(R.id.full_image);
+
+            //  ---- FULL IMAGE PARAMS ----
+            ConstraintLayout.LayoutParams fullImageParams = new ConstraintLayout.LayoutParams((20/100)*width,(kflamounty/kflamountx)*(20/100)*width);
+            fullImageParams.topToTop = R.id.Level_layout;
+            fullImageParams.rightToRight = R.id.Level_layout;
+            fullImageParams.rightMargin = 16;
+            fullImageParams.topMargin = 16;
+            fullImage.setLayoutParams(fullImageParams);
+            fullImage.setImageResource(R.drawable.lvl3_1_2);
+
+            //  ---- PREVIOUS BUTTON PARAMS ----
+            ConstraintLayout.LayoutParams previousParams = new ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            previousParams.leftToLeft = R.id.Level_layout;
+            previousParams.bottomToBottom = R.id.Level_layout;
+            previousParams.leftMargin = 16;
+            previousParams.bottomMargin = 16;
+            previous.setLayoutParams(previousParams);
+            previous.setText("Previous");
+            previous.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View view) {
+                    window.previousOnclick(previous);
+                }
+            });
+
+            //  ---- NEXT BUTTON PARAMS ----
+            ConstraintLayout.LayoutParams nextParams = new ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            nextParams.rightToRight = R.id.Level_layout;
+            nextParams.bottomToBottom = R.id.Level_layout;
+            nextParams.bottomMargin = 16;
+            nextParams.rightMargin = 16;
+            next.setLayoutParams(nextParams);
+            next.setText("Next");
+            next.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View view) {
+                    window.nextOnclick(next);
+                }
+            });
+
+            //  ---- START BUTTON PARAMS ----
+            ConstraintLayout.LayoutParams startParams = new ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            startParams.leftToRight = R.id.previous;
+            startParams.rightToLeft = R.id.next;
+            startParams.bottomToBottom = R.id.Level_layout;
+            startParams.bottomMargin = 16;
+            start.setLayoutParams(startParams);
+            start.setText("Start");
+            start.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View view) {
+                    window.startOnclick(start);
+                }
+            });
+
+            //  ---- COUNTER PARAMS ----
+            ConstraintLayout.LayoutParams counterParams = new ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            counterParams.topToBottom = R.id.full_image;
+            counterParams.bottomToTop = R.id.next;
+            counter.setLayoutParams(counterParams);
+            counter.setTextSize(14);
+
+            //  ---- ADD CONSTANT WIDGETS TO LAYOUT ----
+            lvlLayout.addView(previous);
+            lvlLayout.addView(next);
+            lvlLayout.addView(start);
+            lvlLayout.addView(fullImage);
+            lvlLayout.addView(counter);
+
+
+        //  ---- SETTING KAFELS ----
 
         for (int i = 0; i < kafelNo; i++)
             buttArr[i] = new ImageButton(window);
@@ -68,15 +163,19 @@ class LvlMng {
         for (int i = 0; i < kafelNo; i++)
             lvlLayout.addView(buttArr[i]);
 
-/*        for (int i = 0; i < kflamounty; i++) {
+        ConstraintLayout.LayoutParams kafelParams = new ConstraintLayout.LayoutParams(edge, edge);
+        kafelParams.leftToLeft = R.id.Level_layout;
+        kafelParams.topToTop = R.id.Level_layout;
+/*
+        for (int i = 0; i < kflamounty; i++) {
             for (int j = 0; j < kflamountx; j++) {
                 int index = i * kflamountx + j;
                 int leftMargin = minmarginx + j * edge + j * space;
                 int topMargin = minmarginy + i * edge + i * space;
-                constraintSet.connect(buttArr[index].getId(), constraintSet.LEFT, constraintSet.PARENT_ID, constraintSet.LEFT, leftMargin);
-                constraintSet.applyTo(lvlLayout);
-                constraintSet.connect(buttArr[index].getId(), constraintSet.TOP, constraintSet.PARENT_ID, constraintSet.TOP, topMargin);
-                constraintSet.applyTo(lvlLayout);
+                kafelParams.leftMargin = leftMargin;
+                kafelParams.topMargin = topMargin;
+                buttArr[index].setLayoutParams(kafelParams);
+                buttArr[index].setImageResource(R.color.colorAccent);
             }
         }*/
     }
