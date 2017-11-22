0e53fc201fff2d66b704da085331da53d301516e
jacek-bulatek <jacek.bulatek@gmail.com>
2017-11-22 20:13:52 +0100

    [#vmq7] Disable animation for double-click on the same tile

--- app/src/main/java/com/example/nockanakalinowej/View/TilesMatrixLayout.java
+++ app/src/main/java/com/example/nockanakalinowej/View/TilesMatrixLayout.java
@@ -141,8 +141,10 @@ class TilesMatrixLayout extends ConstraintLayout {
         clickedTile.setBackgroundResource(R.drawable.tile_cover_selected);
         clickedTileIDs[action] = clickedTile.getId();
         if(action == 1){
-            animationManager.startAnimation(findViewById(clickedTileIDs[0]), findViewById(clickedTileIDs[1]));
-
+            if (clickedTileIDs[0] != clickedTileIDs [1])
+                animationManager.startAnimation(findViewById(clickedTileIDs[0]), findViewById(clickedTileIDs[1]));
+            else
+                findViewById(clickedTileIDs[0]).setBackgroundResource(R.drawable.tile_cover);
         }
         action = 1 - action;
     }
